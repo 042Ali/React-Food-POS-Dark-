e9104b864cd69143783bdd766bb75e2979de53dd
042Ali <ali20090609@gmail.com>
2024-10-05 18:38:59 +0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Menu from "./components/Menu/Menu";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Menu />
+    </div>
+  );
+}
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,9 @@
+import "./Menu.css";
+
+export default function Menu() {
+  return (
+    <div className="Menu__Container">
+      <h1>okefiokdos</h1>
+    </div>
+  );
+}
